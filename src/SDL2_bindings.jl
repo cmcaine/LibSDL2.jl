8cbaf65b8af1288fbf73f046203818ebfa354693
Colin Caine <cmcaine@gmail.com>
2020-05-23 05:46:51 +0100

    Files generated by PkgTemplates

---
+++ src/SDL2_bindings.jl
@@ -0,0 +1,5 @@
+module SDL2_bindings
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SDL2_bindings
+using Test
+
+@testset "SDL2_bindings.jl" begin
+    # Write your own tests here.
+end
